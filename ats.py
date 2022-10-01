547cfe7f4034d089c0d8cc6f8cbe36653d8a3b81
Bruna Oliveira <69090175+bruliveira@users.noreply.github.com>
2022-10-01 20:57:28 -0300

    Falta umas coisas

--- ats.py
+++ ats.py
@@ -19,42 +19,37 @@ def server(host = 'localhost', port=8082):
 
     atual = datetime.now()
     hora = atual.strftime('%H:%M:%S')
-    hora_data = atual.strftime('%d/%m/%Y /%H:%M:%S')
+    hora_data = atual.strftime('%d/%m/%Y - %H:%M:%S')
     data = atual.strftime('%d/%m/%Y')
-
 
     while True:
         print ("Esperando mensagem do cliente")
         client, address = sock.accept()
         op = client.recv(data_payload)
-        '''
-        o que fazer com a mensagem recebida ?
-        '''
-        if(op == 1):
-            print("-"*10, "Data", "-"*10,)
-            print("gfg",data)
-        if(op == 2):
-            print("-"*10, "Hora", "-"*10,)
-            print(hora)
-        if(op == 3):
-            print("-"*10, "Data e Hora", "-"*10,)
-            print(hora_data)
-        if(op == 0):
-            print("Tchauu")     
+        ver = (int(op) + 1) - 1
 
-
-
-
-
-
-      
-        
-
-
-
-
-
-        
-
+        #o que fazer com a mensagem recebida ?
+    
+        if op:
+            if(ver == 1):
+                print("-"*10, "Data (Opção 1)", "-"*10,)
+                print("->", data)
+                #print("1 - valor do ver:", ver)
+                #print("Ver: ", ver, "opção: ", op)
+                client.send(op)
+                client.close()
+            if(ver == 2):
+                print("-"*10, "Hora (Opção 2)", "-"*10,)
+                print("->", hora)
+                client.send(op)
+                client.close()
+            if(ver == 3):
+                print("-"*10, "Data e Hora (Opção 3)", "-"*10,)
+                print("->", hora_data)
+                client.send(op)
+                client.close()
+            if(op == 0):
+                print("Tchauu")
+                client.send(op)
+                client.close()
 server()
-
