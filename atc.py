490b7a4bd90c3971b3cf43026b669f21b8ebdabd
AdrianaSil <96041964+AdrianaSil@users.noreply.github.com>
2022-10-03 02:29:10 -0300

    Add files via upload
    Correção do código

--- atc.py
+++ atc.py
@@ -1,20 +1,19 @@
 #Solicita - Data, Hora, Data e hora
 import socket
+
 def client(host = 'localhost', port=8082):
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-    server_address = (host, port)
+    server_address = (host, port) #destino
     print ("Conectando %s porta %s" % server_address)
     sock.connect(server_address)
     
     try:
-        op = input("O que você deseja:\n1 - Data\n2 - Hora\n3 - Data e Hora\n")
-        
-        print ("Enviando opção %s" % op)
-        
-        sock.sendall(op.encode('utf-8'))
-        op = sock.recv(2048)
+        op = input("Consultar:\n1 - Data\n2 - Hora\n3 - Data e Hora\n")
+        sock.send(op.encode('utf-8'))
+        print(str(sock.recv(2048).decode()))
+        print('\n')
     except socket.error as e:
         print ("Socket error: %s" %str(e))
     except Exception as e:
@@ -22,5 +21,4 @@ def client(host = 'localhost', port=8082):
     finally:
         print ("Closing connection to the server")
         sock.close()
-
 client()
--- ats.py
+++ ats.py
@@ -1,51 +1,39 @@
-from datetime import date
+import socket
+import threading
 from datetime import datetime
 
-import socket
+
+def atender_requisicao(client):
+    data = client.recv(2048).decode() #msg recebida do cliente
+    match data:
+        case "1":
+            print('teste')
+            date = str(datetime.today().strftime("%d/%m/%Y"))
+            client.send(date.encode('utf-8'))
+        case "2":
+            date = str(datetime.now().strftime("%H:%M"))
+            client.send(date.encode('utf-8'))
+        case "3":
+            date = str(datetime.now().strftime("%d/%m/%Y %H:%M"))
+            client.send(date.encode('utf-8'))
+    client.close()
+
 
 def server(host = 'localhost', port=8082):
-    data_payload = 2048 
-    
-    sock = socket.socket(socket.AF_INET,  socket.SOCK_STREAM)
-    
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    
+
+    sock = socket.socket(socket.AF_INET,  socket.SOCK_STREAM) #criando socket
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #garante que o socket seja destruido
+
     server_address = (host, port)
     print ("Iniciando servidor na porta %s %s" % server_address)
-    sock.bind(server_address)
-    
-    sock.listen(1)
-    i = 0
 
-    atual = datetime.now()
-    hora = atual.strftime('%H:%M:%S')
-    hora_data = atual.strftime('%d/%m/%Y - %H:%M:%S')
-    data = atual.strftime('%d/%m/%Y')
-
+    sock.bind(server_address) #associa o socket a uma porta
+    sock.listen(5)
+ 
     while True:
-        print ("Esperando mensagem do cliente")
-        client, address = sock.accept()
-        op = client.recv(data_payload)
-        ver = (int(op) + 1) - 1
+        print ("Esperando solicitação do cliente")
+        client, serv = sock.accept()
 
-        #o que fazer com a mensagem recebida ?
-    
-        if op:
-            if(ver == 1):
-                print("-"*10, "Data (Opção 1)", "-"*10,)
-                print("->", data)
-                #print("1 - valor do ver:", ver)
-                #print("Ver: ", ver, "opção: ", op)
-                client.send(op)
-                client.close()
-            if(ver == 2):
-                print("-"*10, "Hora (Opção 2)", "-"*10,)
-                print("->", hora)
-                client.send(op)
-                client.close()
-            if(ver == 3):
-                print("-"*10, "Data e Hora (Opção 3)", "-"*10,)
-                print("->", hora_data)
-                client.send(op)
-                client.close()
+        t1 = threading.Thread(target = atender_requisicao,  args=(client,))
+        t1.start()
 server()
